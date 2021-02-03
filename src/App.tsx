91325685e53d3b31c29d453a203a3ea5f93c91b7
William Gigliozzi <gigliozzi@gmail.com>
2021-02-03 15:12:41 -0300

    Add Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
+import Navbar from './core/components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Navbar />
+)
 
 export default App;
---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Navbar = () => (
+    <nav className="main-navbar">
+        <div className="logo-container">
+            <a href="xxx" className="nav-logo-text">
+                <h4 >
+                    Bootcamp DevSuperior
+                </h4>
+            </a>
+        </div>
+    </nav>
+)
+
+export default Navbar;
